e15c3411af8179310a6a719d094341932d1c3685
shiddieqy <shabriashiddieqy@gmail.com>
2023-05-26 15:25:23 +0700

    DATA KELAR

--- kalkulasi_final_boss_3.m
+++ kalkulasi_final_boss_3.m
@@ -1,14 +1,13 @@
 record = 51;
-% res =  [zeros(3,4) zeros(3,4) zeros(3,4) zeros(3,4) zeros(3,4) zeros(3,4) zeros(3,4) zeros(3,4) zeros(3,4) zeros(3,4) zeros(3,4) zeros(3,4) zeros(3,4) zeros(3,4)];
-res = zeros(6,14);
-minsec =20;
-maxsec = 32;
+res =  zeros(6,14);
+minsec = 16;
+maxsec = 28;
 sampfreq = 48000;
 soundspeed = 343;
 nf = 0.008;
 Ts = 1/sampfreq;
 ts = minsec:Ts:maxsec;
-n = 500;
+n = 3000;
 W1 = 1000/sampfreq;
 W2 = 1400/sampfreq;
 Wn = [W1 W2];
@@ -17,8 +16,8 @@ pros = 1;
 for seq = 1:14
     for mic1 = 1:3
         for mic2 = (mic1+1):4
-            Audio1 = audioread("FIltered_3"+"/ZOOM00"+string(record)+"_Tr"+string(mic1)+".WAV");
-            Audio2 = audioread("Filtered_3"+"/ZOOM00"+string(record)+"_Tr"+string(mic2)+".WAV");
+            Audio1 = audioread("FIltered_7.2"+"/ZOOM00"+string(record)+"_Tr"+string(mic1)+".WAV");
+            Audio2 = audioread("Filtered_7.2"+"/ZOOM00"+string(record)+"_Tr"+string(mic2)+".WAV");
             Audio1 = normalize(Audio1);
             Audio2 = normalize(Audio2);
 %             Audio1 = filter(b,1,Audio1);
@@ -32,7 +31,7 @@ for seq = 1:14
 
             lag = finddelay(Audio1,Audio2);
             distance = lag/sampfreq*soundspeed;
-            res((pros-(seq-1)*6),seq)=distance;
+            res(pros-(seq-1)*6,seq)=distance;
             fprintf("Process : %d / 84\n",pros)
             pros = pros+1;
         end
